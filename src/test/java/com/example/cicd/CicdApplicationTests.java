3ac29bfe16492934118f3ce41679e1e9da4bd6b2
go2github <jsheik.career@gmail.com>
2022-07-16 22:49:15 +0530

    'precommit'

--- src/test/java/com/example/cicd/CicdApplicationTests.java
+++ src/test/java/com/example/cicd/CicdApplicationTests.java
@@ -1,13 +1,23 @@
 package com.example.cicd;
 
+import com.example.cicd.service.WelcomeService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 @SpringBootTest
 class CicdApplicationTests {
 
+    @Autowired
+    WelcomeService welcomeService;
+
     @Test
     void contextLoads() {
+        String msg=welcomeService.welcome("sheik");
+        assertEquals("sheik welcome to cicdj",msg);
     }
 
 }
