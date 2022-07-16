82b046eada57e863f73e4222146ab9491d0977cd
go2github <jsheik.career@gmail.com>
2022-07-17 00:36:32 +0530

    'sonarqube'

--- src/test/java/com/example/cicd/CicdApplicationTests.java
+++ src/test/java/com/example/cicd/CicdApplicationTests.java
@@ -17,7 +17,7 @@ class CicdApplicationTests {
     @Test
     void contextLoads() {
         String msg=welcomeService.welcome("sheik");
-        assertEquals("sheik welcome to cicdlhf",msg);
+        assertEquals("sheik welcome to cicd",msg);
     }
 
 }
--- src/test/java/com/example/cicd/g.java
+++
@@ -1,2 +0,0 @@
-package com.example.cicd;public class g {
-}
